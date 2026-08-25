c369797ace986ee16f42f72fdbc24ae5dd1f9e6b
hoseony <hoseon.alex.yu@gmail.com>
2026-08-24 22:20:38 -0700

    finished board from_fen

--- fish/src/board/parse.rs
+++ fish/src/board/parse.rs
@@ -14,9 +14,9 @@ impl Board {
         };
 
         let mut parts = fen.split_whitespace();
-        // 1. let's put down some pieces
-
-        let placement = parts.next().expect("from_fen | missing piece placement");
+        
+        // 1. place pieces
+        let placement: &str = parts.next().expect("from_fen | missing piece placement");
         // fen starts at A8!
         let mut sq: i32 = 56;
 
@@ -44,13 +44,62 @@ impl Board {
                         'r' => (Color::Black, Piece::Rook),
                         'q' => (Color::Black, Piece::Queen),
                         'k' => (Color::Black, Piece::King),
-                        _ => panic!("frome_fen | unexpected character: {}", c),
+                        _ => panic!("frome_fen (1) | unexpected character: {}", c),
                     };
                     // let's place the piece
                     board.pieces[color as usize][piece as usize].set_bit(sq as u8);
                     sq += 1;
                 }
             }
+        }
+
+        // 2. side to move
+        let side: &str = parts.next().expect("from_fen (2) | missing side to move");
+        board.side_to_move = if side == "b" {
+            Color::Black
+        } else {
+            Color::White
+        };
+
+        // 3. castling
+        let castling = parts.next().expect("from_fen (3) | missing castling rights");
+        board.castling_rights = 0;
+
+        for c in castling.chars() {
+            match c {
+                'K' => board.castling_rights |= 0b0001,
+                'Q' => board.castling_rights |= 0b0010,
+                'k' => board.castling_rights |= 0b0100,
+                'q' => board.castling_rights |= 0b1000,
+                '-' => {},
+                _ => panic!("from_fen (3) | unexpected character: {}", c),
+            }
+        }
+
+        // 4. en_passant
+        let ep: &str = parts.next().expect("from fen (4) | missing en passant field");
+        println!("{}", ep);
+
+        board.en_passant = if ep == "-" {
+            None
+        } else {
+            // else, marks it with algebraic notation
+            let bytes: &[u8] = ep.as_bytes();
+            Some((bytes[1] - b'1') * 8 + (bytes[0] - b'a'))
+        };
+
+        // 5. halfmove clock
+        let halfmove: Option<&str> = parts.next();       
+        match halfmove {
+            Some(count) => board.halfmove_clock = count.parse().expect("from fen (5) | Not a valid number"),
+            None => board.halfmove_clock = 0,
+        }
+
+        // 6. full move
+        let fullmove: Option<&str> = parts.next();
+        match fullmove {
+            Some(count) => board.fullmove_number = count.parse().expect("from fen (6) | Not a valid number"),
+            None => board.halfmove_clock = 0,
         }
 
         return board;
